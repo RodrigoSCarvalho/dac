5cc0cec676a23da08404637e009b92a002b522b8
Rodrigo Carvalho <72170590+RodrigoSCarvalho@users.noreply.github.com>
2021-11-06 20:53:43 -0300

    Update HelloServlet.java

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -95,7 +95,7 @@ public class HelloServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet HelloServlet</h1>");
-            out.println("<p>" + msg + "</p>");
+            out.println("<h2>" + msg + "</h2>");
             out.println("</body>");
             out.println("</html>");
         }
